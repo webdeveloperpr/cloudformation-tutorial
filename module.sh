e6966a63e2d0fce5a2ce5c5bf4fe46a5f35c5538
Luis Betancourt <webdeveloperpr@gmail.com>
2020-03-28 12:42:45 -0400

    Add ChangeSets to convert the simple stack into a Load Balanced Stack

--- module.sh
+++ module.sh
@@ -9,13 +9,19 @@ function getInstancesIds() {
     echo $ids
 }
 
+function importParameters() {
+    cat ./config.json |
+        jq '.parameters' |
+        jq 'reduce .[] as $item (""; . + $item.key + "=" + $item.value)'
+}
+
 # Updates a cfn template.
 function updateStack() {
+    parameters = importParameters
     aws cloudformation deploy \
+        --parameter-overrides Key1=Value1 Key2=Value2 \
         --template-file ./ec2.yaml \
         --stack-name simple-example \
         --s3-bucket cf-templates-g4ool1dn68hz-us-east-1 \
         --force-upload
 }
-
-updateStack
