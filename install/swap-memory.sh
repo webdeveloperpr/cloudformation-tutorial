563d5552ef4ee2f4adc01b17941c680f92edf40b
Luis Betancourt <webdeveloperpr@gmail.com>
2020-04-08 02:51:34 -0400

    Update the Jenkins stack

--- install/swap-memory.sh
+++ install/swap-memory.sh
@@ -1,6 +1,15 @@
-sudo dd if=/dev/zero of=/swapfile bs=128M count=32
+################################################################################
+# Swap Memory
+################################################################################
+# Create the swapfile
+sudo dd if=/dev/zero of=/swapfile bs=128M count=16 (2048MB)
+# Set permissions
 sudo chmod 600 /swapfile
+# Make the swap file
 sudo mkswap /swapfile
+# mount the swap file
 sudo swapon /swapfile
+# idk what this is
 sudo swapon -s
+# set the swap memory every time OS boots up.
 sudo su -c "echo /swapfile swap swap defaults 0 0 >> /etc/fstab"
